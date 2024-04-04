08cb2243762a4d0cc96a4b1c02ab65d1d7e7a205
wasicode25 <131186629+wasicode25@users.noreply.github.com>
2024-04-03 22:06:40 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 print("DEMO")
+from flask import Flask
 def mi_funcion():
   a=1
   b=3
@@ -6,3 +7,8 @@ def mi_funcion():
     return 'mayor'
   else
     return 'menor'
+
+
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = secret_key  # Noncompliant
