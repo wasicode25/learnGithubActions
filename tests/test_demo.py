2488f0162f3d882345633c808868adfea36fb133
nriofrio <repositorionicorm@gmail.com>
2024-04-04 14:59:31 -0500

    test

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,10 +1,10 @@
 import sys 
 sys.path.insert(0,'../src')
 
-from src import demo
+from src import main
 def test_demo_menor():
-    resultado=demo.mi_funcion(3,10)
+    resultado=main.mi_funcion(3,10)
     assert resultado=='menor'
 def test_demo_mayor():
-    resultado=demo.mi_funcion(10,3)
+    resultado=main.mi_funcion(10,3)
     assert resultado=='mayor'
