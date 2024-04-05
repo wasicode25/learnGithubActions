247ab3c44ef920296fccc8d6d0133540080bef41
nriofrio <repositorionicorm@gmail.com>
2024-04-05 16:03:16 -0500

    tox

--- servipagos/tests/test_demo.py
+++ servipagos/tests/test_demo.py
@@ -5,6 +5,6 @@ from bronze import main
 def test_demo_menor():
     resultado=main.mi_funcion(3,10)
     assert resultado=='menor'
-#def test_demo_mayor():
-    #resultado=main.mi_funcion(10,3)
-    #assert resultado=='mayor'
+def test_demo_mayor():
+    resultado=main.mi_funcion(10,3)
+    assert resultado=='mayor'
