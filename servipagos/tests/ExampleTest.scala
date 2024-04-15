1d3c5971370e6e317b4c29dd9180fc3dd47256e6
nriofrio <repositorionicorm@gmail.com>
2024-04-14 21:58:38 -0500

    sonar properties x

--- servipagos/tests/ExampleTest.scala
+++ servipagos/tests/ExampleTest.scala
@@ -2,7 +2,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ExampleTest extends AnyFlatSpec with Matchers {
-  "Example" should "sum two numbers" in {
-    Example.sum(1, 5) shouldBe 6
+  "La función sum" should "devolver la suma de dos números" in {
+    val resultado = Example.sum(3, 5)
+    resultado shouldEqual 8
+  }
+
+  "La función ExampleIssues" should "no tener problemas de estilo" in {
+    // Verificar que la lista está vacía
+    Example.exampleIssues()
+    val isEmpty = List(1).isEmpty
+    isEmpty shouldBe true
+
+    // Verificar que 'a' es un entero
+    val a = 14 / 1
+    a shouldBe a.toInt
+
+    // Verificar que 'b' es un booleano
+    val b = if (true) true else false
+    b shouldBe aBoolean
+
+    // Verificar que 'two' es 0 después del bucle
+    var two = 1 + 1
+    while (two > 0) {
+      println("WOW!")
+      two -= 1
+    }
+    two shouldBe 0
   }
 }
